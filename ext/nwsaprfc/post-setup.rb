c41e522fc7208efd4eec1f6ab75bdbee50c06781
Arndt Lehmann <alehmann@jpn.tuv.com>
2015-11-04 11:58:10 +0900

    Fix 'uninitialized constant Installer::Config' error

--- ext/nwsaprfc/post-setup.rb
+++ ext/nwsaprfc/post-setup.rb
@@ -1,3 +1,3 @@
 require "rbconfig.rb"
 
-system("mt -outputresource:nwsaprfc.so;2 -manifest nwsaprfc.so.manifest") if "mswin32".eql? Config::CONFIG["host_os"] and FileTest.exists?("nwsaprfc.so.manifest")
+system("mt -outputresource:nwsaprfc.so;2 -manifest nwsaprfc.so.manifest") if "mswin32".eql? RbConfig::CONFIG["host_os"] and FileTest.exists?("nwsaprfc.so.manifest")
